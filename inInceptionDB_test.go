209fc4043b84548a8717f22f43668916a4683002
fulldump <gerardooscarjt@gmail.com>
2025-03-17 01:15:01 +0100

    fix: inceptiondb run in docker

--- inInceptionDB_test.go
+++ inInceptionDB_test.go
@@ -1,15 +1,27 @@
 package persistence
 
 import (
+	"context"
 	"testing"
+
+	"github.com/google/uuid"
 )
 
 func TestInInception(t *testing.T) {
 
-	p := NewInInceptionDB[Item](&ConfigInceptionDB{
-		Base:       "http://localhost:1212/v1",
-		Collection: "test",
-	})
+	collection := "testing-" + uuid.NewString()
+	var p *InInceptionDB[Item]
+
+	for _, base := range []string{"http://inceptiondb:1212/v1", "http://localhost:1212/v1"} {
+		p = NewInInceptionDB[Item](&ConfigInceptionDB{
+			Base:       base,
+			Collection: collection,
+		})
+		_, err := p.List(context.Background())
+		if err == nil {
+			break
+		}
+	}
 
 	SuitePersistencer(p, t)
 	// SuiteOptimisticLocking(p, t) // working on this!
